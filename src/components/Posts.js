cb33975b091d9c26ef62fa5606675b18e7afffa5
Elijah <delosreyes.elijah@gmail.com>
2020-09-09 22:08:59 -0700

    implementing redux

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -12,21 +12,26 @@ export class Posts extends Component {
     }
 
     componentDidMount() {
+        this.getPosts();
+    }
+
+    getPosts = () => {
         axios
             .get(
                 URL +
-                    "/object?apikey=9b7b63e1-aa03-4f5d-8d56-f2c58a6a4efe&size=100"
+                    // "/object?apikey=9b7b63e1-aa03-4f5d-8d56-f2c58a6a4efe&page=1&size=100"
+                    "/classification?apikey=9b7b63e1-aa03-4f5d-8d56-f2c58a6a4efe&size=100"
             )
             .then(
                 (res) => {
-                    // console.log(res.data.records);
+                    // console.log(res.data);
                     this.setState({ posts: res.data.records });
                 },
                 (err) => {
                     console.log(err);
                 }
             );
-    }
+    };
 
     render() {
         return (
--- src/index.js
+++ src/index.js
@@ -3,6 +3,46 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// Redux
+import { createStore } from "redux";
+
+// STORE
+
+// ACTIONS
+const getWorks = () => {
+    return {
+        type: "GETWORKS",
+    };
+};
+const getList = () => {
+    return {
+        type: "GETLIST",
+    };
+};
+
+// REDUCERS
+const works = (state = [], action) => {
+    switch (action.type) {
+        case "GETWORKS":
+            return []; // get data from api
+    }
+};
+const list = (state = [], action) => {
+    switch (action.type) {
+        case "GETLIST":
+            return []; // get list from api
+    }
+};
+
+// STORE
+let store = createStore(works);
+
+// Display store to console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(getWorks());
+
 ReactDOM.render(
     <React.StrictMode>
         <App />
