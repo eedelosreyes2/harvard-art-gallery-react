b52ab50df427908c1738a65180ff6ca4256485fe
Elijah <delosreyes.elijah@gmail.com>
2020-09-17 20:52:21 -0700

    removed inline css header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./css/App.css";
 
 import Header from "./components/Header";
 import Works from "./components/Works";
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,77 +1,75 @@
 import React, { Component } from "react";
+import "../css/Header.css";
 
 export class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tabs: [
-                {
-                    id: 0,
-                    title: "Classification",
-                    list: [
-                        "(not asigned)",
-                        "Accessories (non-art)",
-                        "Albums",
-                        "Amulets",
-                        "Architectural",
-                        "Elements",
-                        "Archival Materials",
-                        "Artists' Tools",
-                        "Audiovisual Works",
-                        "Books",
-                        "Boxes",
-                        "Calligraphy",
-                        "Cameos",
-                        "Coins",
-                        "Drawings",
-                        "Fragments",
-                        "Furnishings",
-                        "Furniture",
-                        "Gems",
-                        "Graphic Design",
-                    ],
-                },
-                {
-                    id: 1,
-                    title: "Work Type",
-                    list: ["work"],
-                },
-                {
-                    id: 2,
-                    title: "Technique/Medium",
-                    list: ["teqc"],
-                },
-                {
-                    id: 3,
-                    title: "Period",
-                    list: [],
-                },
-                {
-                    id: 4,
-                    title: "Place",
-                    list: [],
-                },
-                {
-                    id: 5,
-                    title: "Century",
-                    list: [],
-                },
-                {
-                    id: 6,
-                    title: "Culture",
-                    list: [],
-                },
-                {
-                    id: 7,
-                    title: "Gallery",
-                    list: [],
-                },
-            ],
-            showLists: false,
-            currentId: -1,
-            currentList: [],
-        };
-    }
+    state = {
+        tabs: [
+            {
+                id: 0,
+                title: "Classification",
+                list: [
+                    "(not asigned)",
+                    "Accessories (non-art)",
+                    "Albums",
+                    "Amulets",
+                    "Architectural",
+                    "Elements",
+                    "Archival Materials",
+                    "Artists' Tools",
+                    "Audiovisual Works",
+                    "Books",
+                    "Boxes",
+                    "Calligraphy",
+                    "Cameos",
+                    "Coins",
+                    "Drawings",
+                    "Fragments",
+                    "Furnishings",
+                    "Furniture",
+                    "Gems",
+                    "Graphic Design",
+                ],
+            },
+            {
+                id: 1,
+                title: "Work Type",
+                list: ["work"],
+            },
+            {
+                id: 2,
+                title: "Technique/Medium",
+                list: ["teqc"],
+            },
+            {
+                id: 3,
+                title: "Period",
+                list: [],
+            },
+            {
+                id: 4,
+                title: "Place",
+                list: [],
+            },
+            {
+                id: 5,
+                title: "Century",
+                list: [],
+            },
+            {
+                id: 6,
+                title: "Culture",
+                list: [],
+            },
+            {
+                id: 7,
+                title: "Gallery",
+                list: [],
+            },
+        ],
+        showLists: false,
+        currentId: -1,
+        currentList: [],
+    };
 
     handleDropDown = (id) => {
         var showLists = this.state.showLists;
@@ -90,99 +88,46 @@ export class Header extends Component {
 
     render() {
         return (
-            <div
-                style={{
-                    backgroundColor: "black",
-                    display: "flex",
-                    flexDirection: "column",
-                    position: "fixed",
-                    top: 0,
-                    width: "95%",
-                    zIndex: 9,
-                }}
-            >
+            <div className="header">
                 {/* Above line */}
-                <div
-                    style={{
-                        alignItems: "center",
-                        borderBottom: "1px solid gray",
-                        display: "flex",
-                        height: "50px",
-                        justifyContent: "space-between",
-                    }}
-                >
+                <div className="upper">
                     {/* Harvard Art Museums Logo */}
                     <a
+                        className="logo"
                         href="https://www.harvardartmuseums.org"
                         target="__blank"
                         rel="noopener noreferrer"
                     >
-                        <h1
-                            style={{
-                                display: "inline-block",
-                                fontSize: "30px",
-                            }}
-                        >
-                            H
-                        </h1>
-                        <span
-                            style={{
-                                borderBottom: "1px solid white",
-                                display: "inline-block",
-                                transform:
-                                    "rotate(-45deg) translate(-1px, -11px)",
-                                WebkitTransform:
-                                    "rotate(-45deg) translate(-1px, -11px)",
-                                width: "40px",
-                            }}
-                        />
+                        <h1 className="h">H</h1>
+                        <span className="diag-line" />
                     </a>
 
                     {/* About and Mode */}
-                    <div style={{ alignItems: "center", display: "flex" }}>
-                        <a
-                            href="https://github.com/eedelosreyes2/harvard-art-gallery-react"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            style={{ marginRight: "15px" }}
-                        >
-                            About
-                        </a>
-                        <span>Mode</span>
-                    </div>
+                    <ul className="about-mode">
+                        <li>
+                            <a
+                                href="https://github.com/eedelosreyes2/harvard-art-gallery-react"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                About
+                            </a>
+                        </li>
+                        <li>Mode</li>
+                    </ul>
                 </div>
 
                 {/* Below line */}
-                <div
-                    style={{
-                        alignItems: "flex-start",
-                        display: "flex",
-                        height: "40px",
-                        justifyContent: "space-between",
-                        paddingTop: "10px",
-                        width: "100%",
-                    }}
-                >
+                <div className="lower">
                     {/* Tabs */}
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "row",
-                            flexWrap: "wrap",
-                            paddingTop: "5px",
-                        }}
-                    >
+                    <div className="tabs">
                         {this.state.tabs.map((tab) => {
                             const { id, title, list } = tab;
                             return (
                                 <div
+                                    className="tab"
                                     key={id}
                                     onClick={() => this.handleDropDown(id)}
-                                    style={{
-                                        cursor: "pointer",
-                                        marginBottom: "10px",
-                                        marginRight: "25px",
-                                    }}
                                 >
                                     {title}
                                 </div>
@@ -191,25 +136,15 @@ export class Header extends Component {
                     </div>
 
                     {/* Searchbar */}
-                    <div style={{ alignItems: "center", display: "flex" }}>
-                        <input
-                            placeholder="Search by keyword, title, artist, or gallery"
-                            style={{
-                                background: "transparent",
-                                border: "none",
-                                color: "white",
-                                height: "20px",
-                                outline: "none",
-                                textAlign: "right",
-                                width: "250px",
-                            }}
-                        />
+                    <div className="search-bar">
+                        <input placeholder="Search by keyword, title, artist, or gallery" />
                     </div>
                 </div>
 
                 {/* Lists */}
                 {this.state.showLists ? (
                     <div
+                        className="list"
                         style={{
                             backgroundColor: "rebeccapurple",
                             height: "40px",
