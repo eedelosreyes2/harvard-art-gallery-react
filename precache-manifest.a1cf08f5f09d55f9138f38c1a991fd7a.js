d85bb7e00abde3eb1bd15094d70aced4e7825107
Elijah <delosreyes.elijah@gmail.com>
2020-09-22 21:02:56 -0700

    Updates

---
+++ precache-manifest.a1cf08f5f09d55f9138f38c1a991fd7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc761cc599153f6a62ff11852a83fd9",
+    "url": "/harvard-art-gallery-react/index.html"
+  },
+  {
+    "revision": "db9c4b27af5a8cd8960d",
+    "url": "/harvard-art-gallery-react/static/css/main.7845349e.chunk.css"
+  },
+  {
+    "revision": "8c8682497218137dc487",
+    "url": "/harvard-art-gallery-react/static/js/2.9d3a936f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/harvard-art-gallery-react/static/js/2.9d3a936f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db9c4b27af5a8cd8960d",
+    "url": "/harvard-art-gallery-react/static/js/main.3fda52d1.chunk.js"
+  },
+  {
+    "revision": "e31920e9cdcf942627a5",
+    "url": "/harvard-art-gallery-react/static/js/runtime-main.cb74d310.js"
+  }
+]);
